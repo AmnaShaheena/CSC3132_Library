d939b79fdb03dc81e5c8ba5e519ce254a4017a8e
Amna Shaheena <119941495+AmnaShaheena@users.noreply.github.com>
2024-12-18 08:21:03 +0000

    commit

---
+++ link.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>library</title>
+</head>
+<body>
+    <?php
+    require_once 'conf/dbconf.php';
+    require_once 'func/func.php';
+
+    //create a page to display all available books
+    echo $_SERVER['PHP_SELF'];//get the file name
+?>
+ <?php
+
+joinTable($connect);
+    
+ 
+ ?> 
+    
+</body>
+</html>
